58250c251e871821487ebe4dd7afa4d7f2b7f6d3
webdevaadil <75753195+webdevaadil@users.noreply.github.com>
2022-07-22 16:08:39 +0530

    thid

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-const dotenv=require("dotenv")
+const dotenv = require("dotenv");
+const path = require("path");
 dotenv.config({ path: "./config.env" });
 const express = require("express");
-const connectDB =require("./config/db")
-const path = require("path")
-connectDB()
+const cookie = require("cookie-parser");
+const session = require("express-session")
+const bodyparser = require("body-parser")
+// const redis = require("redis")
+// const reisStore=require("connect-redis")(session)
+// const client=redis.createClient()
+
+// const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
+const ErrorResponse = require("./utlis/errorresponse");
+connectDB();
 
 const app = express();
+app.use(cookie())
 
-app.use(express.json())
-app.use('/api/auth', require('./routes/auth'))
-const PORT = process.env.PORT ||5000;
+app.use(express.json());
+app.use("/api/auth", require("./routes/auth"));
+// app.use("/api/private", require("./routes/private"));
 
+//error handle
 
 // --------------------------deployment------------------------------
 if (process.env.NODE_ENV === "production") {
@@ -19,8 +30,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
-} 
-else {
+} else {
   app.get("/", (req, res) => {
     res.send("API is running..");
   });
@@ -28,13 +38,13 @@ else {
 
 
 // --------------------------deployment------------------------------
+app.use(ErrorResponse)
+const PORT = process.env.PORT || 5000;
 
-
-
-const server=app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
 });
-process.on("unhandledRejection",(err,promise)=>{
+process.on("unhandledRejection", (err, promise) => {
   console.log(` logged error${err}`);
-  server.close(()=>process.exit(1));
-})
+  server.close(() => process.exit(1));
+});
