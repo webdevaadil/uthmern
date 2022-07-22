19d52a7d3cfef4db7fe940624e31e19bedd14fc3
aashutosh402 <chandrawanshiaashutosh@gmail.com>
2022-07-22 13:08:54 +0530

    a

---
+++ client/build/precache-manifest.1df9c8b5a78258bdea0250d4dfcd0fba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a3d86b65ca4d42a75ad",
+    "url": "/static/css/main.0af93899.chunk.css"
+  },
+  {
+    "revision": "9a3d86b65ca4d42a75ad",
+    "url": "/static/js/main.9a3d86b6.chunk.js"
+  },
+  {
+    "revision": "aec6667541f03fa9fda0",
+    "url": "/static/js/1.aec66675.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7bb603ecc1609f5a54415eb55cd135c8",
+    "url": "/static/media/Rectangle1.7bb603ec.jpg"
+  },
+  {
+    "revision": "b6f7246bc55c8ee752b78e99be8783ae",
+    "url": "/static/media/goat-slide.b6f7246b.png"
+  },
+  {
+    "revision": "755b60c8540500d7a9090c474cc40259",
+    "url": "/static/media/shhh.755b60c8.png"
+  },
+  {
+    "revision": "c8952dd8a7c25a38b32f9cc7f711f67c",
+    "url": "/static/media/welcome-img.c8952dd8.png"
+  },
+  {
+    "revision": "67c5f4a617527413e5a2db8dd90939aa",
+    "url": "/index.html"
+  }
+];
